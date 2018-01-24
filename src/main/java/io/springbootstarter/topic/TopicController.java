f762f6d2517a5ef0f976a5dfdd328d77573f314d
Vasundhara Chimmad <vasudha.c8@gmail.com>
2018-01-24 14:28:39 -0800

    Accessing a single resource

--- src/main/java/io/springbootstarter/topic/TopicController.java
+++ src/main/java/io/springbootstarter/topic/TopicController.java
@@ -1,11 +1,11 @@
 package io.springbootstarter.topic;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TopicController {
@@ -18,4 +18,10 @@ public class TopicController {
 		return topicService.getAllTopics();
 
 	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id){
+	    return topicService.getTopic(id);
+
+    }
 }
